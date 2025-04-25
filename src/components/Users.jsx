cbf79b86708ab3caae6271d3d3c55a69b5eedc4c
jnakht <jisan.cse.cu@gmail.com>
2025-04-25 20:16:31 +0600

    user deleted from database successfully, but stll remain on UI without refreshing, and remain on firebase

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,17 +1,43 @@
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
 
 const Users = () => {
     const loadedUsers = useLoaderData();
     const [users, setUsers] = useState(loadedUsers);
+
+    const handleDeleteUser = (_id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/users/${_id}`, {
+                    method: 'DELETE'
+                })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "The user has been deleted.",
+                            icon: "success"
+                        });
+                    }
+                })
+            }
+        });
+    }
     return (
         <div>
             <h3>No of users: {loadedUsers.length}</h3>
-
-
-
-
-
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
@@ -30,7 +56,7 @@ const Users = () => {
                                     <th>2</th>
                                     <td>{user.email}</td>
                                     <td>{user?.createdAt}</td>
-                                    <td>Purple</td>
+                                    <td><button onClick={() => handleDeleteUser(user._id)} className="btn btn-error">Delete</button></td>
                                 </tr>
                             )
                         }
